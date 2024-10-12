9d8ecfb432d067064341184256244f09063f471e
Nabiur siddique <nabiursiddique01@gmail.com>
2024-10-12 14:33:23 +0600

    Filter is added

--- searching-filtering-client/src/App.jsx
+++ searching-filtering-client/src/App.jsx
@@ -1,34 +1,24 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Users } from './users'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
+  const [query, setQuery] = useState('');
+  // console.log(Users.filter(user => user.first_name.toLowerCase().includes('fe')));
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+      <h1 className="text-4xl font-bold text-center my-10">Searching And Filtering</h1>
+
+      <div className="w-1/3 mx-auto">
+        <label className="input input-bordered flex items-center gap-2">
+          <input type="text" className="grow" placeholder="Search" onChange={(e) => setQuery(e.target.value)} />
+        </label>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="text-center text-2xl mt-3">
+        <ul className="list">
+          {Users.filter(user => user.first_name.toLowerCase().includes(query)).map((user) => <li key={user.id}>{user.first_name}</li>)}
+        </ul>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
